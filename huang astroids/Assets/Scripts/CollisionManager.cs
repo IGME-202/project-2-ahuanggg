ad9485faffc710506d3157e2c7c91c3167d455f7
ahuanggg <ah6595@rit.edu>
2020-10-10 14:24:39 -0400

    got moving vehicle and collision detection

---
+++ huang astroids/Assets/Scripts/CollisionManager.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum CollisionCheckMethod
+{
+    AABB,
+    Circle
+}
+
+public class CollisionManager : MonoBehaviour
+{
+    [SerializeField]
+    List<SpriteRenderer> objects = new List<SpriteRenderer>();
+
+    public CollisionCheckMethod checkMethod;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            switch(checkMethod)
+            {
+                case CollisionCheckMethod.AABB:
+                    checkMethod = CollisionCheckMethod.Circle;
+                    break;
+                case CollisionCheckMethod.Circle:
+                    checkMethod = CollisionCheckMethod.AABB;
+                    break;
+            }
+        }
+
+        bool isPlayerHit = false;
+
+        for (int i = 1; i < objects.Count; ++i)
+        {
+            if(CheckForCollision(objects[0], objects[i], checkMethod))
+            {
+                objects[i].color = Color.red;
+
+                isPlayerHit = true;
+                //objects[0].color = Color.red;
+            }
+            else
+            {
+                objects[i].color = Color.white;
+
+                //objects[0].color = Color.white;
+            }
+        }
+
+        if (isPlayerHit)
+        {
+            objects[0].color = Color.red;
+        }
+        else
+        {
+            objects[0].color = Color.white;
+        }
+
+    }
+
+    bool CheckForCollision(SpriteRenderer objectA, SpriteRenderer objectB, CollisionCheckMethod collisionCheck)
+    {
+        bool isHitting = false;
+
+        switch(collisionCheck)
+        {
+            case CollisionCheckMethod.AABB:
+                if (objectB.bounds.min.x < objectA.bounds.max.x
+                    && objectB.bounds.max.x > objectA.bounds.min.x
+                    && objectB.bounds.max.y > objectA.bounds.min.y
+                    && objectB.bounds.min.y < objectB.bounds.max.y)
+                {
+                    isHitting = true;
+                }
+                break;
+            case CollisionCheckMethod.Circle:
+                if (Vector3.Distance(objectA.bounds.center, objectB.bounds.center) < (objectA.bounds.size.magnitude + objectB.bounds.size.magnitude) / 2f)
+                {
+                    isHitting = true;
+                }
+                break;
+
+        }
+
+
+        return isHitting;
+    }
+
+}
---
+++ huang astroids/Assets/Scripts/Vehicle.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vehicle : MonoBehaviour
+{
+
+    Vector3 vehiclePosition = Vector3.zero;
+
+    [SerializeField]
+    Vector3 direction = Vector3.right;
+
+    Vector3 velocity = Vector3.zero;
+
+    [SerializeField]
+    float speed = 0.00001f;
+
+    [SerializeField]
+    float turnSpeed = 0.0001f;
+
+    public float maxSpeed = 1f;
+    
+
+    Camera cam;
+    public float width;
+    public float height;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+        height = 2f * cam.orthographicSize;
+        width = height * cam.aspect;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            //accelerate or speed increases
+            speed += 0.01f;
+        }
+        if (Input.GetKeyUp(KeyCode.UpArrow))
+        {
+            while (speed >= 0.000001f)
+            { 
+                speed *= .95f;
+            }
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            //turn left
+            if (turnSpeed < 5f)
+            {
+                turnSpeed += 0.2f;
+            }
+            else
+            {
+                turnSpeed = 5;
+            }
+            //direction += Vector3.left;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            while (turnSpeed >= 0.000001f)
+            {
+                turnSpeed *= .95f;
+            }
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            //turn right
+            if (turnSpeed > -5f)
+            {
+                turnSpeed -= 0.2f;
+            }
+            else
+            {
+                turnSpeed = -5;
+            }
+
+            //direction += Vector3.right;
+        }
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            while (turnSpeed <= -0.000001f)
+            {
+                turnSpeed *= .95f;
+            }
+        }
+
+
+        direction = Quaternion.Euler(0, 0, turnSpeed) * direction;
+
+        velocity = direction * speed;
+
+        vehiclePosition += velocity;
+
+        transform.position = vehiclePosition;
+
+        transform.rotation = Quaternion.LookRotation(Vector3.forward, direction);
+
+        if(vehiclePosition.x > 9)
+        {
+            vehiclePosition.x = -9;
+        }
+        if (vehiclePosition.x < -9)
+        {
+            vehiclePosition.x = 9;
+        }
+
+
+        if (vehiclePosition.y > 5)
+        {
+            vehiclePosition.y = -5;
+        }
+        if (vehiclePosition.y < -5)
+        {
+            vehiclePosition.y = 5;
+        }
+    }
+}
